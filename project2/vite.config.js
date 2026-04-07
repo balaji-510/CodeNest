ac2a2b02945d4cf9fd5b1e4f4808d20b02ac9056
balaji-rcrm <balaji@recruitcrm.io>
2026-04-08 02:33:12 +0530

    Add _redirects file for React Router support on Render

--- project2/vite.config.js
+++ project2/vite.config.js
@@ -4,6 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  publicDir: 'public',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  },
   server: {
     proxy: {
       '/api/codechef': {
